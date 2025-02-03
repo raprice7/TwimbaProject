787bcfe1bf0335f612a443312dc785dcb9fdf7da
Austin Price <111824191+raprice7@users.noreply.github.com>
2025-02-03 12:58:02 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export const tweetsData = [   
     {
         handle: `@TrollBot66756542 💎`,
-        profilePic: `images/troll.jpg`,
+        profilePic: `./images/troll.jpg`,
         likes: 27,
         retweets: 10,
         tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
